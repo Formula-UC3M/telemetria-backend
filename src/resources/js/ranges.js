1dd437b21d1e145c31955772dbe91b2b10f83364
sebastiancbvz <sebastian.cabanas@cbiconsulting.es>
2018-05-18 03:35:56 +0200

    Modificar ranges js para redirigir automaticamente a login si no hay token.

--- src/resources/js/ranges.js
+++ src/resources/js/ranges.js
@@ -1,28 +1,16 @@
 (() => {
 	const authToken = localStorage.getItem('token');
-
+	
 	if (!authToken) {
 		return window.location.href = '/login';
 	}
-	
+
 	function renderForm(data) {
 		
 	}
 
 	function setEvents() {
-		const formRanges = document.getElementById('ranges-form');
-
-		formRanges.addEventListener('submit', event => {
-			event.preventDefault();
-
-			fetch('/users/signup', {
-				method: 'POST',
-				body: new FormData(formRanges)
-			})
-			.then(res => res.json())
-			.then(data => localStorage.setItem('token', data.token))
-			.catch(error => alert(error));
-		});
+		
 	}
 
 	document.addEventListener('DOMContentLoaded', event => {	
@@ -34,7 +22,6 @@
 		})
 		.then(res => res.json())
 		.then(data => {
-			console.log(data);
 			renderForm(data);
 		})
 		.catch(error => alert(error));
