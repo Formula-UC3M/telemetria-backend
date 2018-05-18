bf8b623a857f606c7432f056daafd7fb139bab8b
sebastiancbvz <sebastian.cabanas@cbiconsulting.es>
2018-05-18 03:19:52 +0200

    Añadir routas de la api de rangos.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const routes = require('pillars').routes;
 const main = require('./main');
 const data = require('./data');
 const users = require('./users');
+const ranges = require('./ranges');
 
 console.info('Añadiendo rutas al servidor.');
 routes.add(data);
 routes.add(users);
+routes.add(ranges);
 routes.add(main);
