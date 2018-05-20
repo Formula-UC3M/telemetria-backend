b3c8b487d15c9dd655c2992835bce9a9890c4279
sebastiancbvz <sebastian.cabanas@cbiconsulting.es>
2018-05-20 14:01:01 +0200

    Añadir frontend como dependencia del back y instalarlo con npm :P

--- build.sh
+++ build.sh
@@ -2,14 +2,3 @@
 npm install
 npm run lint
 npm run test
-
-[ -e telemetria-frontend ] && rm -rf telemetria-frontend
-git clone -b dev --depth 1 https://github.com/Formula-UC3M/telemetria-frontend.git
-cd telemetria-frontend
-npm install
-npm run build
-[ -e dist ] && mv dist/ ../public/
-
-# Back to origin
-cd ..
-exit
